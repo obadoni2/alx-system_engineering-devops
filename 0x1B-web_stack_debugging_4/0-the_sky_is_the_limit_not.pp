36b7874aaac8f324993dad19e2f2892fdb591a0d
obadoni2 <obadoniemma@gmail.com>
2024-08-23 12:25:45 +0100

    jhkk

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,13 @@
-# This manuscript increase the amount of traffic an Nginx server can handle
-# increase the  ULIMIT of the default file
+# Increases the amount of traffic an Nginx server can handle.
 
-exec {'fix--for-nginx':
-   command => 'sed -i "s/15/4096/" /etc/default/nginx
-   path    => '/usr/local/bin/:/bin/'
-    
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-}
-
-#Restart Nginx
--> exec { 'nginx-restart':
-    command => 'nginx restart':
-    path => '/etc/init.d/'
-    
-
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,16 +1,13 @@
-#this manuscript enable the user holberton to login and oprn file without error
+# Enable the user holberton to login and open files without error.
 
-#increase hard file limit for user holberton
-
+# Increase hard file limit for Holberton user.
 exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.confi',
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
-
 }
 
-#increase soft file limit for user holberton
-exec{ 'increase-soft-file-limit-for-holberton-user
- command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
- path => '/usr/local/bin/:/bin/'
-
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
